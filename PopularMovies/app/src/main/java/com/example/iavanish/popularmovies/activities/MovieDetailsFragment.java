e68c704f384900e6e4b7d7b4a3ebf12e24cc694b
iavanish <avanish@jeeves.co.in>
2016-09-16 05:42:40 +0530

    almost done

--- PopularMovies/app/src/main/java/com/example/iavanish/popularmovies/activities/MovieDetailsFragment.java
+++ PopularMovies/app/src/main/java/com/example/iavanish/popularmovies/activities/MovieDetailsFragment.java
@@ -6,14 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.iavanish.popularmovies.R;
 import com.example.iavanish.popularmovies.db.AccessDatabase;
 import com.example.iavanish.popularmovies.entities.MoviesList;
+import com.example.iavanish.popularmovies.util.JSONParser;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class MovieDetailsFragment extends Fragment {
 
@@ -21,7 +32,8 @@ public class MovieDetailsFragment extends Fragment {
     private View view;
     private ImageView movieThumbnail;
     private CheckBox favourite;
-    private TextView showDetails;
+    private static List<String> trailers;
+    private static List<String> reviews;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,7 +45,6 @@ public class MovieDetailsFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_movie_details, container, false);
         movieThumbnail = (ImageView) view.findViewById(R.id.movieThumbnail);
         favourite = (CheckBox) view.findViewById(R.id.favourite);
-        showDetails = (TextView) view.findViewById(R.id.showDetails);
 
         final MoviesList movies = MoviesList.getInstance();
 
@@ -43,14 +54,6 @@ public class MovieDetailsFragment extends Fragment {
         movieThumbnail.setScaleType(ImageView.ScaleType.FIT_XY);
         String posterURL = getResources().getString(R.string.bigPosterURL) + movies.movies.get(movieIndex).getPoster_path();
         Picasso.with(context).load(posterURL).into(movieThumbnail);
-
-        StringBuilder str = new StringBuilder("\n\n");
-        str.append("Original Title: " + movies.movies.get(movieIndex).getOriginal_title() + "\n\n");
-        str.append("Overview: " + movies.movies.get(movieIndex).getOverview() + "\n\n");
-        str.append("Vote Average: " + movies.movies.get(movieIndex).getVote_average() + "\n\n");
-        str.append("Release Date: " + movies.movies.get(movieIndex).getRelease_date() + "\n\n");
-
-        showDetails.setText(str.toString());
 
         if(new AccessDatabase(context).isMoviePresentInDB(movies.movies.get(movieIndex))) {
             favourite.setChecked(true);
@@ -68,7 +71,91 @@ public class MovieDetailsFragment extends Fragment {
             }
         });
 
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.baseLinearLayout);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        TextView textView = getTextView("Original Title: " + movies.movies.get(movieIndex).getOriginal_title());
+        linearLayout.addView(textView);
+
+        textView = getTextView("Overview: " + movies.movies.get(movieIndex).getOverview());
+        linearLayout.addView(textView);
+
+        textView = getTextView("Vote Average: " + movies.movies.get(movieIndex).getVote_average());
+        linearLayout.addView(textView);
+
+        textView = getTextView("Release Date: " + movies.movies.get(movieIndex).getRelease_date());
+        linearLayout.addView(textView);
+
+        String url = context.getResources().getString(R.string.base_url) + String.valueOf(movies.movies.get(movieIndex).getId()) +
+                context.getResources().getString(R.string.trailer_url) + context.getResources().getString(R.string.apiKey);
+
+        RequestQueue queue1 = Volley.newRequestQueue(getActivity());
+        StringRequest stringRequest1 = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                trailers = new JSONParser().getTrailers(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.err.println("Something went wrong while retrieving data from theMovieDB!");
+            }
+        });
+        queue1.add(stringRequest1);
+
+        if(trailers != null) {
+            for (String temp : trailers) {
+                Button button = getButton(temp);
+                linearLayout.addView(button);
+            }
+        }
+
+        url = context.getResources().getString(R.string.base_url) + String.valueOf(movies.movies.get(movieIndex).getId()) +
+                context.getResources().getString(R.string.review_url) + context.getResources().getString(R.string.apiKey);
+
+        RequestQueue queue2 = Volley.newRequestQueue(getActivity());
+        StringRequest stringRequest2 = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                trailers = new JSONParser().getReviews(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.err.println("Something went wrong while retrieving data from theMovieDB!");
+            }
+        });
+        queue2.add(stringRequest2);
+
+        if(reviews != null) {
+            for (String temp : reviews) {
+                textView = getTextView(temp);
+                linearLayout.addView(textView);
+            }
+        }
+
         return view;
+    }
+
+    private TextView getTextView(String text) {
+        TextView textView = new TextView(context);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        params.setMargins(5,60,5,0);
+        textView.setLayoutParams(params);
+        textView.setTextAppearance(context, android.R.style.TextAppearance_Medium);
+        textView.setText(text);
+        return textView;
+    }
+
+    private Button getButton(String text) {
+        Button button = new Button(context);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        params.setMargins(5,60,5,0);
+        button.setLayoutParams(params);
+        button.setTextAppearance(context, android.R.style.TextAppearance_Medium);
+        button.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        button.setText(text);
+        return button;
     }
 
     @Override
--- PopularMovies/app/src/main/java/com/example/iavanish/popularmovies/util/JSONParser.java
+++ PopularMovies/app/src/main/java/com/example/iavanish/popularmovies/util/JSONParser.java
@@ -6,6 +6,9 @@ import com.example.iavanish.popularmovies.entities.MoviesList;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by iavanish on 16/9/16.
  */
@@ -34,4 +37,41 @@ public class JSONParser {
         return movies;
     }
 
+    public List<String> getTrailers(String response) {
+        List<String> trailers = new ArrayList<>();
+        try {
+            JSONObject json_object = new JSONObject(response);
+            JSONArray json_array = json_object.getJSONArray("results");
+            int n = json_array.length();
+            for(int i = 0; i < n; i++) {
+                JSONObject tempJSON = json_array.getJSONObject(i);
+                String key = tempJSON.getString("key");
+                trailers.add(key);
+            }
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return trailers;
+    }
+
+    public List<String> getReviews(String response) {
+        List<String> reviews = new ArrayList<>();
+        try {
+            JSONObject json_object = new JSONObject(response);
+            JSONArray json_array = json_object.getJSONArray("results");
+            int n = json_array.length();
+            for(int i = 0; i < n; i++) {
+                JSONObject tempJSON = json_array.getJSONObject(i);
+                String author = tempJSON.getString("author");
+                String content = tempJSON.getString("content");
+                String review = author + "\n" + content;
+                reviews.add(review);
+            }
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return reviews;
+    }
 }
